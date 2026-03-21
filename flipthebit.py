81b9643e73ab6ce2cce7142b120e28890e1495a5
patilmalhar108 <patilmalhar.a@gmail.com>
2026-03-21 06:34:56 -0500

    Add files via upload

---
+++ flipthebit.py
@@ -0,0 +1,14 @@
+def total_flips(number1, number2):
+    flips = 0
+    while number1 > 0 or number2 > 0:
+        t1 = number1 & 1
+        t2 = number2 & 1
+        if t1 != t2:
+            flips += 1
+        number1 >>= 1
+        number2 >>= 1
+    return flips
+
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+print("Number of flips needed:",total_flips(number1,number2))
